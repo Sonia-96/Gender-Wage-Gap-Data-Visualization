51c990dfb91654eb262453d372e8e89a4f2f62a1
Sonia-96 <672365639@qq.com>
2023-07-30 00:14:51 -0600

    adjust legend color & add legend

--- app.js
+++ app.js
@@ -11,13 +11,61 @@ window.onload = async function() {
                     countryData = topojson.feature(data, data.objects.countries).features; // convert topojson to GeoJson
                     console.log(wageData);
                     drawMap(2020);
+                    drawLegend();
+                    drawSlider();
                 }
             } 
         )
     })
 }
 
-const slider = d3.sliderHorizontal()
+async function loadData() {
+    let text = await d3.text("employee_wage_gap.json");
+    let jsonData = JSON.parse(text);
+    for (let year in jsonData) {
+        wageData[year] = {};
+        for (key in jsonData[year]) {
+            wageData[year][key] = jsonData[year][key];
+        }
+    }
+}
+
+const legendData = [10, 20, 50, 100, 500]
+const legendColor = ['#fef0d9','#fdcc8a','#fc8d59','#e34a33','#b30000']
+
+function drawLegend() {
+    const colorScale = d3.scaleOrdinal().domain(legendData).range(legendColor);
+
+    // Create the legend
+    const legendSvg = d3.select("#legend").attr("width", 200).attr("height", 50);
+
+    const legendWidth = 180; // Width of the legend excluding margins
+    const legendHeight = 20; // Height of the color rectangles
+    const legendMargin = { top: 10, left: 10 };
+
+    // Create color rectangles in the legend
+    const legendColors = legendSvg.selectAll("rect")
+    .data(legendData) // Adjust the range based on your data values
+    .enter()
+    .append("rect")
+    .attr("x", (d, i) => legendMargin.left + (i * (legendWidth / 5)))
+    .attr("y", legendMargin.top)
+    .attr("width", legendWidth / 3)
+    .attr("height", legendHeight)
+    .attr("fill", d => colorScale(d));
+
+    // Add text labels beside the color rectangles
+    const legendLabels = legendSvg.selectAll("text")
+    .data(legendData) // Adjust the range based on your data values
+    .enter()
+    .append("text")
+    .attr("x", (d, i) => legendMargin.left + (i * (legendWidth / 5)) + 20)
+    .attr("y", legendMargin.top + legendHeight + 15)
+    .text(d => d);
+}
+
+function drawSlider() {
+    const slider = d3.sliderHorizontal()
     .min(1970)
     .max(2020)
     .step(1)
@@ -30,23 +78,13 @@ const slider = d3.sliderHorizontal()
         drawMap(val);
     })
 
-d3.select('#slider')
-    .append('svg')
-    .attr('width', 1200)
-    .attr('height', 100)
-    .append('g')
-    .attr('transform', 'translate(30,30)')
-    .call(slider);
-
-async function loadData() {
-    let text = await d3.text("employee_wage_gap.json");
-    let jsonData = JSON.parse(text);
-    for (let year in jsonData) {
-        wageData[year] = {};
-        for (key in jsonData[year]) {
-            wageData[year][key] = jsonData[year][key];
-        }
-    }
+    d3.select('#slider')
+        .append('svg')
+        .attr('width', 1200)
+        .attr('height', 100)
+        .append('g')
+        .attr('transform', 'translate(30,30)')
+        .call(slider);
 }
 
 function getCountry(year, id) {
@@ -68,12 +106,11 @@ function getWageGap(year, id) {
 function fillColor(value) {
     if (value == 0) {
         return '#ccc';
-    } else if (value < 10) {
-        return 'orange';
-    } else if (value < 20) {
-        return 'tomato';
-    } else {
-        return 'red';
+    }
+    for (let i = 0; i < legendData.length; i++) {
+        if (value < legendData[i]) {
+            return legendColor[i];
+        }
     }
 }
 
@@ -122,11 +159,5 @@ function drawMap(year) {
 function cleanMap() {
     const map = d3.select("#world-map");
     map.remove();
-}
-
-function drawLegend() {
-    const svg = d3.select('#map').append('svg');
-    labels = ["< 10", "< 20", ">20"];
-
 }
 
