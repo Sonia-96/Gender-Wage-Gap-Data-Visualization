a45b881e99587d2c810da26c74d73c0e2bba662d
Lauryn Claire Hansen <lauryn.c.hansen@gmail.com>
2023-08-02 23:52:50 -0600

    Draft for bar chart, only x-axis displaying.

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ window.onload = async function() {
                     drawMap(2020);
                     drawLegend();
                     drawSlider();
+                    drawBarChart();
                 }
             } 
         )
@@ -178,3 +179,48 @@ function cleanMap() {
     map.remove();
 }
 
+function drawBarChart() {
+// set the dimensions and margins of the graph
+    var margin = {top: 20, right: 30, bottom: 40, left: 90},
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+    var svg = d3.select("#barchart")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+//Add X axis
+    var x = d3.scaleLinear()
+        .domain([1970, 2020])
+        .range([ 0, 300]);
+        svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+
+//Y axis
+    var y = d3.scaleBand()
+        .range([ 0, height ])
+        .domain(wageData.map(function(d) { return d.name; }))
+        .padding(.1);
+        svg.append("g")
+        .call(d3.axisLeft(y))
+
+//Bars
+    svg.selectAll("myRect")
+        .data(wageData)
+        .enter()
+        .append("rect")
+        .attr("x", x(0) )
+        .attr("y", function(d) { return y(d.name); })
+        .attr("width", function(d) { return x(d.value); })
+        .attr("height", y.bandwidth() )
+        .attr("fill", "#69b3a2")
+
+}
